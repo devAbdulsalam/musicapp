3e465abc14554eac0474c27cd8239f96fe2e57d5
my-amtech <ammuftau74@gmail.com>
2023-06-29 15:42:16 +0100

    Fix cors errors

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,34 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(cookieParser());
-var cors = require('cors')
-var path = require('path')
+var cors = require('cors');
+var path = require('path');
 
+require('dotenv').config();
 
-	require("dotenv").config();
+app.use(
+	cors({
+		origin: ['http://localhost:3000', 'https://music-api-o7q6.onrender.com'],
+		credentials: true,
+	})
+);
+app.use(express.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(
+	bodyParser.urlencoded({
+		limit: '50mb',
+		extended: true,
+		parameterLimit: 50000,
+	})
+);
 
-app.use(cors())
-app.use(express.json({limit:'50mb'}));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({  
-    limit:'50mb' ,
-extended: true ,
- parameterLimit:50000}));
+const songs = require('./routes/songRouter');
+const user = require('./routes/userRouter');
 
-
-const songs  = require('./routes/songRouter');
-const user  = require('./routes/userRouter');
-
-
-app.use("/api/v1/", songs);
-app.use("/api/v1/", user);
-
+app.use('/api/v1/', songs);
+app.use('/api/v1/', user);
 
 // app.use(express.static(path.join(__dirname, "../frontend/build")));
 
